e9df1b4fcd76fb014ab4c1c1a26f5806ea38dcb9
James Sample <jamesgsample@gmail.com>
2022-12-20 09:42:12 +0000

    Creating Artists

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,3 +1,15 @@
-exports.create = (req, res) => {
-    res.sendStatus(201);
-}
+// exports.create = (req, res) => {
+//     res.sendStatus(201);
+// }
+
+
+exports.create = async (req, res) => {
+    const { name, genre } = req.body
+  
+    try {
+      const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
+      res.status(201).json(artist)
+    } catch (err) {
+      res.status(500).json(err.message)
+    }
+  }
--- tests/artist-create.test.js
+++ tests/artist-create.test.js
@@ -1,19 +1,27 @@
-// tests/artist-create.js
 const { expect } = require('chai');
-const request = require('supertest');
-const app = require('../src/app');
+ const request = require('supertest');
+ const db = require('../src/db');
+ const app = require('../src/app');
 
-describe('create artist', () => {
-  describe('/artists', () => {
-    describe('POST', () => {
-      it('creates a new artist in the database', async () => {
-        const res = await request(app).post('/artists').send({
-          name: 'Tame Impala',
-          genre: 'rock',
-        });
+ describe('create artist', () => {
+   describe('/artists', () => {
+     describe('POST', () => {
+       it('creates a new artist in the database', async () => {
+         const { status, body } = await request(app).post('/artists').send({
+           name: 'Tame Impala',
+           genre: 'rock',
+         });
 
-        expect(res.status).to.equal(201);
-      });
-    });
-  });
-});
+         expect(status).to.equal(201);
+         expect(body.name).to.equal('Tame Impala');
+         expect(body.genre).to.equal('rock');
+
+         const {
+           rows: [artistData],
+         } = await db.query(`SELECT * FROM Artists WHERE id = ${body.id}`);
+         expect(artistData.name).to.equal('Tame Impala');
+         expect(artistData.genre).to.equal('rock');
+       });
+     });
+   });
+ });
